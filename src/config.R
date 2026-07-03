ceef9a54caefe2e265cf97dca4d09c5372da65b3
Dillon Fitch-Polse <dtfitch@ucdavis.edu>
2026-07-03 15:01:32 -0700

    Update config.R
    removed secondary targets library call

--- src/config.R
+++ src/config.R
@@ -7,5 +7,3 @@ setwd("C:/Users/Dillon/Box/_Projects/Caltrans_BC2")
 # The pipeline runs with the working directory set to the Box project root so
 # that data paths ("data_raw/...") and the _targets store resolve to Box.
 tar_source("C:/Users/Dillon/projects/california-atbc-tool-data-pipeline/src/functions")
-
-library(targets)
